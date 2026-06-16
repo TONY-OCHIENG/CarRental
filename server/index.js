8294d27339e26ee6515c4c9eeb4ebafcae03798e
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-16 18:27:00 +0300

    Setting up express server and folders structure

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+dotenv.config()
+
+const app = express()
+//middlewares
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json())
+
+
+app.listen(process.env.PORT, () => {
+    console.log("server is running")
+})

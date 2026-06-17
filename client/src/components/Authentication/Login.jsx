90e52f7c04208d601936454f769c0e7bcd021c75
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-17 11:07:27 +0300

    Adding a function that submits login details to the backend

--- client/src/components/Authentication/Login.jsx
+++ client/src/components/Authentication/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-
+import axios from 'axios'
 function Login() {
   const [values, setValues] = useState({
     email:'',
@@ -15,14 +15,22 @@ function Login() {
       [name] : value
     }))
   }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    axios.post('http://localhost:3000/auth/loginAdmin',values)
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((error) => (console.log(error)))
+  }
   return (
     <div className='bg-gray-100 flex justify-center items-center px-4 h-screen'>
         <div className='p-4 rounded-md bg-white shadow-md w-full md:w-[40%]'>
-            <form action="">
+            <form action="" onSubmit={handleSubmit}>
                 <label htmlFor="username" className='text-gray-600 text-lg'>Email</label>
-                <input type="email" id='username' name='email' className='border-red-700 p-2 w-full rounded-md mb-2 border' placeholder='johndoe@gmail.com' required/>
+                <input type="email" id='username' onChange={handleValues} name='email' className='border-red-700 p-2 w-full rounded-md mb-2 border' placeholder='johndoe@gmail.com' required/>
                 <label htmlFor="password" className='text-gray-600 text-lg'>Password</label>
-                <input type="password" id='password' name='password' className='border-red-700 p-2 w-full rounded-md mb-2 border' required/>
+                <input type="password" id='password' onChange={handleValues} name='password' className='border-red-700 p-2 w-full rounded-md mb-2 border' required/>
                 <button className='w-full py-2 rounded-md cursor-pointer text-red-700 bg-gray-900 text-lg mt-2 font-bold'>Login</button>
             </form>
         </div>

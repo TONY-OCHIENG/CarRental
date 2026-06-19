c7c6136324dabb1f499dd4b73e4d9ba020b5cb0d
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-19 21:11:55 +0300

    Adding navigation links on admin dashboard

--- client/src/pages/Adminpages/Layout.jsx
+++ client/src/pages/Adminpages/Layout.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { Outlet, useNavigate } from 'react-router-dom'
-
+import { Link, NavLink, Outlet, useNavigate } from 'react-router-dom'
+import { CalendarCheck, CarFront, CreditCard, FileText, LayoutDashboard, Locate, MessageSquare, MessageSquareQuote, PowerCircle, Wrench } from 'lucide-react'
 function Layout() {
   const navigate = useNavigate()
   const [auth,setAuth] = useState(false)
@@ -25,12 +25,50 @@ function Layout() {
     let navigateLogin = () => {
       navigate('/login')
     }
+    const dashBoardLinks = [
+      {name:"Dashboard",path:"/admin", icon: LayoutDashboard},
+      {name:"Vehicle",path:"/admin/vehicle", icon: CarFront},
+      {name:"Bookings",path:"/admin/booking", icon: CalendarCheck},
+      {name:"Payments",path:"/admin/payment", icon: CreditCard},
+      {name:"Reports",path:"/admin/report", icon: FileText},
+      {name:"Message",path:"/admin/message", icon: MessageSquare},
+      {name:"Feedbacks",path:"/admin/feedback", icon: MessageSquareQuote},
+      {name:"Maintenance",path:"/admin/maintenance", icon: Wrench},
+      {name:"Track vehicle",path:"/admin/track", icon: Locate},
+    ]
   return (
     <div>
       {
-        auth  && <div>
-              <h1>Layout</h1>
-              <Outlet/>
+        auth  && 
+        <div className='w-full h-[100vh] bg-gray-100 flex'>
+              <div className='h-[100vh] bg-white border-r w-[18%] md:w-[16%]'>
+                <h1 className='text-center md:block hidden mt-4 font-extrabold text-2xl'>ROUTE
+                  <span className='text-red-600'>CAB</span></h1>
+                  <div className='mt-4 w-full'>
+                    {
+                      dashBoardLinks.map((link,index) => (
+                          <NavLink key={index} to={link.path} end className={({isActive}) => `relative flex items-center
+                              max-md:justify-center gap-2 w-full py-2.5 min-md:pl-10 first:mt-6
+                              text-gray-900 ${isActive && 'bg-red-600 text-white group'}`}>
+                              {({isActive}) => (
+                                  <>
+                                    <link.icon className='w-5 h-5'/>
+                                    <p className='max-md:hidden'>{link.name}</p>
+                                    <span className={`w-1.5 h-10 rounded-1 right-0 absolute ${isActive && 'bg-white'}`}/>
+                                  </>
+                              )}
+                        </NavLink>
+                      ))
+                    }
+                      <div className='min-md:pl-10 pl-5 text-gray-900 mt-2.5'>
+                        <p className='flex gap-2 '><PowerCircle/> <span className='max-md:hidden'>Logout</span></p>
+                      </div>
+                    
+                  </div>
+              </div>
+              <div>
+                 <Outlet/>
+              </div>
         </div> 
       }
     </div>

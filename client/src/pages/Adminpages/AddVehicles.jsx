9bac90cbb580f7fc6d9a62069c62c47c9d0cf3d7
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-22 15:36:41 +0300

    Adding a function that submits values to the database

--- client/src/pages/Adminpages/AddVehicles.jsx
+++ client/src/pages/Adminpages/AddVehicles.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 function AddVehicles() {
   const [value, setValues] = useState({
@@ -15,21 +16,38 @@ function AddVehicles() {
       [name] : value
     }))
   }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const formData = new FormData()
+    formData.append('name',value.name)
+    formData.append('capacity',value.capacity)
+    formData.append('price',value.price)
+    formData.append('type',value.type)
+    formData.append('image',value.image)
+    
+    axios.post('http://localhost:3000/auth/addvehicles',formData)
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
   return (
     <div className='py-16 px-10 h-full w-full flex justify-center mt-10'>
       <div className='md:w-[50%] w-full'>
         <h1 className='text-center font-bold text-2xl text-gray-800'>Add vehicles</h1>
-        <form action="" className='w-full rounded-md bg-white p-4'>
+        <form action="" onSubmit={handleSubmit} className='w-full rounded-md bg-white p-4'>
             <label htmlFor="name" className='text-gray-600'>Vehicle name</label>
-            <input type="text" id='name' name='name' className='w-full p-2 border rounded-md mb-2'/>
+            <input type="text" onChange={handleValueChange} id='name' name='name' className='w-full p-2 border rounded-md mb-2'/>
             <label htmlFor="capacity" className='text-gray-600'>Vehicle capacity</label>
-            <input type="number" id='capacity' name='capacity' className='w-full p-2 border rounded-md mb-2'/>
+            <input type="number" onChange={handleValueChange} id='capacity' name='capacity' className='w-full p-2 border rounded-md mb-2'/>
             <label htmlFor="price" className='text-gray-600'>Vehicle price</label>
-            <input type="number" id='price' name='price' className='w-full p-2 border rounded-md mb-2'/>
+            <input type="number"  onChange={handleValueChange} id='price' name='price' className='w-full p-2 border rounded-md mb-2'/>
             <label htmlFor="type" className='text-gray-600'>Vehicle type</label>
-            <input type="text" id='type' name='type' className='w-full p-2 border rounded-md mb-2'/>
+            <input type="text"  onChange={handleValueChange} id='type' name='type' className='w-full p-2 border rounded-md mb-2'/>
             <label htmlFor="image" className='text-gray-600'>Vehicle Image</label>
-            <input type="file" id='image' name='image' className='w-full p-2 border rounded-md'/>
+            <input type="file"  onChange={(event) => {setValues({...value, image: event.target.files[0]})}} id='image' name='image' className='w-full p-2 border rounded-md'/>
             <button type='submit' className='w-full mt-4 cursor-pointer text-white font-bold rounded-md bg-red-600 py-3'>Add vehicle</button>
         </form>
       </div>

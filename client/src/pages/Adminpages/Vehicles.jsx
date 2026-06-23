a320bd94e86c0641e3aa737551ac3aadbce8f006
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-23 13:04:07 +0300

    fetching vehicles from the database

--- client/src/pages/Adminpages/Vehicles.jsx
+++ client/src/pages/Adminpages/Vehicles.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 
 function Vehicles() {
+    const [vehicle,setVehicles] = useState([])
+    const [total,setTotal] = useState(0)
+    const [page, setPage] = useState(1)
+    const limit = 5
+    useEffect(() => {
+      const fetcVehicles = async () => {
+        axios.get(`http://localhost:3000/auth/vehicles?page=${page}&limit=${limit}`)
+        .then((response) => {
+          console.log(response)
+        })
+        .catch((error) => console.log(error))
+      }
+      fetcVehicles()
+    },[page])
     const navigate = useNavigate()
   return (
     <div className='py-20  max-w-7xl md:w-[90%] w-full px-4 mx-auto h-[100vh]'>
         <Link to={'/admin/vehicle/add-vehicle'} className='px-8 py-3 cursor-pointer rounded-md shadow-md bg-red-600 text-white font-bold'>Add Vehicles</Link>
-      <div className='mt-10 w-full h-full'>
-      </div>
+      <div className='mt-10 w-full h-full'></div>
     </div>
   )
 }

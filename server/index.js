130a2208d2ac32248d9e1e1072214e68a79d7f2e
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-23 17:27:24 +0300

    getting a single vehicle function

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ app.use(cors({
 app.use(cookieParser())
 app.use(express.json())
 app.use('/auth',adminRoute)
+app.use(express.static('public'))
 
 app.listen(process.env.PORT, () => {
     console.log("server is running")

ac47f85d2aeba0d2837f84adc147a01419875304
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-23 12:19:00 +0300

    Implementing a function that handles next page

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 function Pagination({page,setPage,total,limit}) {
     const currentPage = page + 1
     const totalPage = Math.ceil( total / limit)
-        
+    const handleNext = () => {
+        if (page < total - 1) {
+            setPage(page + 1)
+        }
+    }    
   return (
     <div>Pagination</div>
   )

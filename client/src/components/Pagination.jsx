82e7ceb0575c20465ffde50386e2371dcfe14c10
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-23 16:57:24 +0300

    Refactoring pagination

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { ArrowLeft, ChevronLeftCircle, ChevronRightCircle} from 'lucide-react'
 
 function Pagination({page,setPage,total,limit}) {
-    const currentPage = page + 1
+    const currentPage = page
     const totalPage = Math.ceil( total / limit)
     const handleNext = () => {
-        if (page < total - 1) {
+        if (page < totalPage) {
             setPage(page + 1)
         }
+        console.log(page)
     }
     const handlePrev = () => {
         if (page > 0) {
@@ -15,14 +16,15 @@ function Pagination({page,setPage,total,limit}) {
         }
     }
     const handleClick = (data) => {
-        setPage(data - 1)
+        setPage(data)
+        console.log(data)
     }    
 
     const renderPages = () => {
         const numbers = []
         const startPage = Math.max(1,currentPage - 3)
         const endPage = Math.min(totalPage,currentPage + 3)
-        for (let i = startPage; i < endPage; i++) {
+        for (let i = startPage; i <= endPage; i++) {
             numbers.push(<button onClick={() => handleClick(i)} key={i}
             className={`w-[35px] h-[35px] rounded-full font-medium flex justify-center items-center
             ${i === currentPage ? "bg-red-600 border text-white" : "text-gray-400 border"}     
@@ -32,10 +34,10 @@ function Pagination({page,setPage,total,limit}) {
         return numbers
     }
   return (
-    <div className='flex justify-between items-center'>
-        <ChevronLeftCircle size={25} onClick={handlePrev} className={`${currentPage === 1 ? 'text-gray-400' : 'text-red-600'}`}/>
+    <div className='flex justify-between items-center md:w-[20%] w-full'>
+        <ChevronLeftCircle size={25} onClick={handlePrev} className={`cursor-pointer ${currentPage === 1 ? 'text-gray-400' : 'text-red-600'}`}/>
          { renderPages() }
-        <ChevronRightCircle size={25} onClick={handleNext} className={`${currentPage === totalPage ? 'text-gray-400': 'bg-red-600'}`}/>
+        <ChevronRightCircle size={25} onClick={handleNext} className={`cursor-pointer ${currentPage === totalPage ?  'text-gray-400': 'text-red-600'}`}/>
     </div>
   )
 }
--- client/src/pages/Adminpages/Vehicles.jsx
+++ client/src/pages/Adminpages/Vehicles.jsx
@@ -1,17 +1,21 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import Pagination from '../../components/Pagination'
 
 function Vehicles() {
     const [vehicle,setVehicles] = useState([])
     const [total,setTotal] = useState(0)
     const [page, setPage] = useState(1)
-    const limit = 5
+    const limit = 3
     useEffect(() => {
       const fetcVehicles = async () => {
         axios.get(`http://localhost:3000/auth/vehicles?page=${page}&limit=${limit}`)
         .then((response) => {
-          console.log(response)
+          if (response.data.status) {
+            setVehicles(response.data.result.result)
+            setTotal(response.data.result.total)
+          }
         })
         .catch((error) => console.log(error))
       }
@@ -21,7 +25,25 @@ function Vehicles() {
   return (
     <div className='py-20  max-w-7xl md:w-[90%] w-full px-4 mx-auto h-[100vh]'>
         <Link to={'/admin/vehicle/add-vehicle'} className='px-8 py-3 cursor-pointer rounded-md shadow-md bg-red-600 text-white font-bold'>Add Vehicles</Link>
-      <div className='mt-10 w-full h-full'></div>
+      <div className='mt-10 w-full h-full'>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-2'>
+          {
+            vehicle.map((item) => (
+              <div className='p-2 bg-white rounded-md shadow-md h-[60vh]'>
+                <img src={`http://localhost:3000/images/`+ item.vehicleImage}  alt="" className='w-full h-[70%] object-cover'/>
+                <h1 className='text-gray-600 font-bold mt-2'>{item.vehicleName}</h1>
+                <h3 className='text-gray-400 text-sm'>{item.vehicleCapacity} seats</h3>
+                <h3 className='text-gray-400 text-sm'>{item.vehicleType} </h3>
+                <h3 className='text-gray-400 text-sm'>{item.vehiclePrice} / day</h3>
+                <button className='w-full rounded-md text-white py-1 mt-1 bg-red-600 cursor-pointer'>Edit</button>
+              </div>
+            ))
+          }
+        </div>
+         <div className='flex justify-center items-center mt-13'>
+         <Pagination page = {page} setPage = {setPage} total = {total} limit = {limit}/>
+        </div>  
+      </div>
     </div>
   )
 }

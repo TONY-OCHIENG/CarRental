a421ead8caafde049d582f3ee8f2462598336e60
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-23 18:32:41 +0300

    Refactoring Edit form

--- client/src/pages/Adminpages/EditVehicle.jsx
+++ client/src/pages/Adminpages/EditVehicle.jsx
@@ -10,7 +10,7 @@ function EditVehicle() {
             axios.get(`http://localhost:3000/auth/vehicles/${id}`)
             .then((response) => {
                 if (response.data.status) {
-                    setVehicle(response.data.result)
+                    setVehicle(response.data.result[0])
                 }
             })
             .catch((error) => console.log(error))
@@ -20,36 +20,35 @@ function EditVehicle() {
   return (
     <div className='py-16 px-10 w-full h-[100vh]'>
         <div className='flex justify-center  w-full '>
-            {
-                vehicle.map((item) => (
-                    <div className='w-full md:w-[50%] bg-white rounded-md shado-md p-4'>
-                       <img src={`http://localhost:3000/images/`+ item.vehicleImage} alt="" />
-                       <form action="">
-                        <div className='flex md:flex-row flex-col  justify-between gap-2'>
-                            <div className='md:w-[50%] w-full'>
-                                <label htmlFor="name">Name</label>
-                                <input type="text" className='border rounded-md w-full p-2'/>
-                            </div>
-                            <div className='md:w-[50%] w-full'>
-                                <label htmlFor="name">Capacity</label>
-                                <input type="number" className='border rounded-md w-full p-2'/>
-                            </div>
+
+                
+                <div className='w-full md:w-[50%] bg-white rounded-md shado-md p-4'>
+                    <img src={`http://localhost:3000/images/`+ vehicle.vehicleImage} alt="" />
+                    <form action="">
+                    <div className='flex md:flex-row flex-col  justify-between gap-2'>
+                        <div className='md:w-[50%] w-full'>
+                            <label htmlFor="name">Name</label>
+                            <input type="text" value={vehicle.vehicleName} onChange={(event) => setVehicle({...vehicle, vehicleName:event.target.value})} className='border rounded-md w-full p-2'/>
                         </div>
-                         <div className='flex md:flex-row flex-col  gap-2'>
-                            <div className='md:w-[50%] w-full'>
-                                <label htmlFor="name">Price</label>
-                                <input type="number" className='border rounded-md w-full p-2'/>
-                            </div>
-                            <div className='md:w-[50%] w-full'>
-                                <label htmlFor="name">Image</label>
-                                <input type="file" className='border rounded-md w-full p-2'/>
-                            </div>
+                        <div className='md:w-[50%] w-full'>
+                            <label htmlFor="name">Capacity</label>
+                            <input type="number" value={vehicle.vehicleCapacity} onChange={(event) => setVehicle({...vehicle, vehicleCapacity:event.target.value})}  className='border rounded-md w-full p-2'/>
                         </div>
-                        <button className='py-2 w-full mt-2 bg-red-600 font-bold text-white rounded-md cursor-pointer'>Edit</button>
-                       </form>
-                    </div>          
-                ))
-            }
+                    </div>
+                        <div className='flex md:flex-row flex-col  gap-2'>
+                        <div className='md:w-[50%] w-full'>
+                            <label htmlFor="name">Price</label>
+                            <input type="number" value={vehicle.vehiclePrice}  onChange={(event) => setVehicle({...vehicle, vehiclePrice:event.target.value})} className='border rounded-md w-full p-2'/>
+                        </div>
+                        <div className='md:w-[50%] w-full'>
+                            <label htmlFor="name">Type</label>
+                            <input type="text"  value={vehicle.vehicleType}  onChange={(event) => setVehicle({...vehicle, vehicleType:event.target.value})} className='border rounded-md w-full p-2'/>
+                        </div>
+                    </div>
+                    <button className='py-2 w-full mt-2 bg-red-600 font-bold text-white rounded-md cursor-pointer'>Edit</button>
+                    </form>
+                </div>          
+             
            
         </div>
     </div>

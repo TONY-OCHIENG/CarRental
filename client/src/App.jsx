5a7fef986d7a0dcddc58f9dc1649a44db3dac4e7
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-25 16:33:16 +0300

    Adding remaining pages on admin dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,12 @@ import Vehicles from './pages/Adminpages/Vehicles'
 import AddVehicles from './pages/Adminpages/AddVehicles'
 import EditVehicle from './pages/Adminpages/EditVehicle'
 import Bookings from './pages/Adminpages/Bookings'
+import Payments from './pages/Adminpages/Payments'
+import Report from './pages/Adminpages/Report'
+import Message from './pages/Adminpages/Message'
+import Feedback from './pages/Adminpages/Feedback'
+import Maintenance from './pages/Adminpages/Maintenance'
+import Track from './pages/Adminpages/Track'
 
 
 
@@ -25,6 +31,12 @@ function App() {
           <Route path='/admin/vehicle/add-vehicle' element={<AddVehicles/>}/>
           <Route path='/admin/vehicle/:id' element={<EditVehicle/>}/>
           <Route path='/admin/bookings' element={<Bookings/>}/>
+          <Route path='/admin/payment' element={<Payments/>}/>
+          <Route path='/admin/report' element={<Report/>}/>
+          <Route path='/admin/message' element={<Message/>}/>
+          <Route path='/admin/feedback' element={<Feedback/>}/>
+          <Route path='/admin/maintenance' element={<Maintenance/>}/>
+          <Route path='/admin/track' element={<Track/>}/>
         </Route>
       </Routes>
     </>

10fec549a84fb699266892e35728a6b4c208e158
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-26 14:44:05 +0300

    Adding a function that handles form submit

--- client/src/pages/user/UserLogin.jsx
+++ client/src/pages/user/UserLogin.jsx
@@ -1,5 +1,7 @@
+import axios from 'axios'
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 function UserLogin() {
     const [userDetails, setUserDetails] = useState({
@@ -13,10 +15,21 @@ function UserLogin() {
             [name] : value
         }))
     }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        axios.post('http://localhost:3000/auth/loginUser',userDetails)
+        .then((response) => {
+            if (response.data.status){
+                toast.success(response.data.message)
+            }
+        })
+        .catch((error) => console.log(error))
+    }
   return (
       <div className='bg-gray-100 flex justify-center items-center px-4 h-screen'>
         <div className='p-4 rounded-md bg-white shadow-md w-full md:w-[40%]'>
-            <form action="">
+            <form action="" onSubmit={handleSubmit}>
                 <label htmlFor="username" className='text-gray-600 text-lg'>Email</label>
                 <input type="email" id='username' onChange={handleValueChange} name='email' className='border-red-700 p-2 w-full rounded-md mb-2 border' placeholder='johndoe@gmail.com' required/>
                 <label htmlFor="password" className='text-gray-600 text-lg'>Password</label>

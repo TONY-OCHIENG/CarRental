7a27970f29937ac099b6e34911a07d7823901ade
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-26 11:04:03 +0300

    Adding a function that submits user's details to the database

--- client/src/pages/user/Register.jsx
+++ client/src/pages/user/Register.jsx
@@ -1,4 +1,6 @@
+import axios from 'axios'
 import React, { useState } from 'react'
+import { toast } from 'react-toastify'
 
 function Register() {
     const [userDetails, setUserDetails] = useState({
@@ -14,10 +16,22 @@ function Register() {
             [name] : value
         }))
     }
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        axios.post('http://localhost:3000/auth/register',userDetails)
+        .then((response) => {
+            if (response.data.status) {
+                toast.success(response.data.message)
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
   return (
     <div className='w-full h-[100vh] bg-gray-100 flex items-center justify-center px-4'>
         <div className='p-4 bg-white rounded-md shadow-md md:w-[40%] w-full'>
-             <form action="">
+             <form action="" onSubmit={handleSubmit}>
                 <label htmlFor="firstname" className='text-gray-600 text-lg'>First name</label>
                 <input type="text" id='firstname'  onChange={handleValue} name='firstname' className='border-red-700 p-2 w-full rounded-md mb-2 border' placeholder='' required/>
                 <label htmlFor="lastname" className='text-gray-600 text-lg'>Last name</label>

b444faa641eb5b5ce275c2127abe60cd125b3c4a
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-26 11:02:08 +0300

    Adding routes that creates users account

--- server/controllers/Usercontrollers.js
+++ server/controllers/Usercontrollers.js
@@ -1,5 +1,17 @@
+import conn from "../configs/db.js"
+import { hashPassword } from "../configs/passwordHash.js"
 
 export const userAccountCreation = (request,response) => {
     const {firstname, lastname, email,password} = request.body
-    console.log(firstname,lastname,email,password)
+    const hpassword = hashPassword(password)
+    try {
+        const sqlQuerry = "INSERT INTO users(firstName,lastName,email,password) VALUES(?,?,?,?)"
+        conn.query(sqlQuerry,[firstname,lastname,email,hpassword],(error,result) => {
+            if (error) return response.status(200).json({status: false, message: error})
+            return response.status(201).json({status: true, message: "Account created successfully"})
+        })
+    } catch (error) {
+        console.log(error)
+        return response.status(500).json({status: false, message: "Internal server error"})        
+    }
 }

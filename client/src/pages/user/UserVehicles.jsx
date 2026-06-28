81b7fb016b8ef81a8809163f56901f6e9b60367f
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-28 17:47:04 +0300

    Refactoring user vehicles page

--- client/src/pages/user/UserVehicles.jsx
+++ client/src/pages/user/UserVehicles.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useState } from 'react'
 
 function UserVehicles() {
+    const [vehicles, setVehicles] = useState([])
+    
   return (
-    <div>UserVehicles</div>
+    <div className='py-16 px-4 w-full h-full'></div>
   )
 }
 

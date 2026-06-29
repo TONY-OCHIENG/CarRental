65bbcb5b79c73ed1881606cb176dfebbb064ca61
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-29 10:41:21 +0300

    Fetching vehicles available for booking from the database

--- client/src/pages/user/UserVehicles.jsx
+++ client/src/pages/user/UserVehicles.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
-
+import axios from 'axios'
 function UserVehicles() {
     const [vehicles, setVehicles] = useState([])
+    useEffect(() => {
+      axios.get('http://localhost:3000/auth/vehiclesBooking')
+      .then((response) => {
+        if (response.data.status) {
+          setVehicles(response.data.result)
+        }
+      })
+      .catch((error) => {console.log(error)})
+    },[])  
     
   return (
     <div className='py-16 px-4 w-full h-full'></div>

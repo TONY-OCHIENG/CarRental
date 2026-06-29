e63ca250f1164c25a8814fdb65a21e7a6664bdf5
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-29 12:08:49 +0300

    Adding single vehicle route

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getvehicles, loginuser, logoutUser, userAccountCreation, userName } from '../controllers/Usercontrollers.js'
+import { getsingleVehicle, getvehicles, loginuser, logoutUser, userAccountCreation, userName } from '../controllers/Usercontrollers.js'
 import { userAuth } from '../middlewares/auth.js'
 
 const userRoutes = express.Router()
@@ -8,4 +8,5 @@ userRoutes.post('/loginUser',loginuser)
 userRoutes.get('/user',userAuth,userName)
 userRoutes.get('/logoutUser',logoutUser)
 userRoutes.get('/vehiclesBooking',getvehicles)
+userRoutes.get('/vehicleSingle',getsingleVehicle)
 export default userRoutes

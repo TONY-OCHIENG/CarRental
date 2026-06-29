0df1b6a3e847839a6f3fa0820711780773134088
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-29 18:10:34 +0300

    Refactoring payment page

--- client/src/pages/user/BookingPage.jsx
+++ client/src/pages/user/BookingPage.jsx
@@ -5,7 +5,6 @@ import { useParams } from 'react-router-dom'
 function BookingPage() {
     const [vehicle, setVehicle] = useState([])
     const { id } = useParams()
-    const [price,setPrice] = useState(1)
     useEffect(() => {
         const fetchSingleVehicle = () => {
             axios.get(`http://localhost:3000/auth/vehicleSingle/${id}`)
@@ -18,13 +17,6 @@ function BookingPage() {
         }
         fetchSingleVehicle()
     },[id])
-    const handleInput = (event) => {
-      const { name, value} = event.target
-      setPrice((prev) => ({
-        ...prev,
-        [name] : value
-      }))
-    }
   return (
     <div className='py-16 max-w-7xl md:w-[90%] mx-auto px-4'>
         <div className='p-4 rounded-md bg-white shadow-md mt-4'>
@@ -37,7 +29,7 @@ function BookingPage() {
                              <h1 className='text-gray-600'>{item.vehiclePrice} / Day</h1>
                              <form action="" className='flex flex-col'>
                                 <label htmlFor="" className='text-gray-600'>Rental days</label>
-                                <input type="number" onChange={handleInput} className='border p-2 rounded-md w-[40%] mt-1'/>
+                                <input type="number" className='border p-2 rounded-md w-[40%] mt-1'/>
                                 <label htmlFor="" className='text-gray-600 mt-2'>Total payout</label>
                                 <input type="number" value={item.vehiclePrice} className='border p-2 rounded-md w-[40%]'/>
                              </form>

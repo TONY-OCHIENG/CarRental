2a1c4c18f4750d93a776350a0118982154e1f92a
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-29 12:16:23 +0300

    Fetching single vehicle details from the database

--- client/src/pages/user/BookingPage.jsx
+++ client/src/pages/user/BookingPage.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 function BookingPage() {
+    const [vehicle, setVehicle] = useState([])
     const { id } = useParams()
+    useEffect(() => {
+        const fetchSingleVehicle = () => {
+            axios.get(`http://localhost:3000/auth/vehicleSingle/${id}`)
+            .then((response) => {
+                if (response.data.status) {
+                    setVehicle(response.data.result)
+                }
+            })
+            .catch((error) => {console.log(error)})
+        }
+        fetchSingleVehicle()
+    },[id])
   return (
     <div>BookingPage</div>
   )

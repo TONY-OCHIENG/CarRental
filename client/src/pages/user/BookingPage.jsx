e64ee1fd84cd04baafd71975632c266c51fd4480
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-29 12:27:43 +0300

    Adding payment section

--- client/src/pages/user/BookingPage.jsx
+++ client/src/pages/user/BookingPage.jsx
@@ -18,7 +18,22 @@ function BookingPage() {
         fetchSingleVehicle()
     },[id])
   return (
-    <div>BookingPage</div>
+    <div className='py-16 max-w-7xl md:w-[90%] mx-auto px-4'>
+        <div className='p-4 rounded-md bg-white shadow-md mt-4'>
+            <div className='md:w-[50%] w-full'>
+                {
+                    vehicle.map((item) => (
+                        <div>
+                             <img src={`http://localhost:3000/images/`+ item.vehicleImage} alt="" className='w-full object-cover' />
+                             <h1 className='text-gray-600 '>{item.vehicleName}</h1>
+                             <h1 className='text-gray-600'>{item.vehiclePrice}</h1>
+                             <button className='py-2 px-12 bg-red-600 text-white cursor-pointer rounded-md cursor-pointer mt-4'>Proceed to payment</button>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    </div>
   )
 }
 

c85df3f28f6aa3a922f62368cdf8921c515ad1a3
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-29 10:56:42 +0300

    Displaying vehicles

--- client/src/pages/user/UserVehicles.jsx
+++ client/src/pages/user/UserVehicles.jsx
@@ -14,7 +14,22 @@ function UserVehicles() {
     },[])  
     
   return (
-    <div className='py-16 px-4 w-full h-full'></div>
+    <div className='py-16 px-4 md:w-[90%] h-[100vh] max-w-7xl mx-auto'>
+      <div className='w-full h-full mt-10 grid grid-cols-1 md:grid-cols-3 gap-2'>
+        {
+          vehicles.length > 0 ? vehicles.map((item) => (
+            <div className='bg-white rounded-md shadow-md p-2 h-[60vh]'>
+                <img src={`http://localhost:3000/images/`+ item.vehicleImage}  alt="" className='w-full h-[70%] object-cover'/>
+                <h1 className='text-gray-600 font-bold mt-2'>{item.vehicleName}</h1>
+                <h3 className='text-gray-400 text-sm'>{item.vehicleCapacity} seats</h3>
+                <h3 className='text-gray-400 text-sm'>{item.vehicleType} </h3>
+                <h3 className='text-gray-400 text-sm'>{item.vehiclePrice} / day</h3>
+                <button className='py-1 mt-2  rounded-md cursor-pointer w-full font-bold text-white bg-red-600'>Book</button>
+            </div>
+          )) : <h1>No available vehicle for booking</h1>
+        }
+      </div>
+    </div>
   )
 }
 

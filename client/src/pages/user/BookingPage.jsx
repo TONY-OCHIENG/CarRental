8b2d575d4882acab5e3994fef608bbaea9d532c3
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-30 16:18:09 +0300

    Adding a function that handles submit of rental details

--- client/src/pages/user/BookingPage.jsx
+++ client/src/pages/user/BookingPage.jsx
@@ -5,7 +5,7 @@ import { useParams } from 'react-router-dom'
 function BookingPage() {
     const [vehicle, setVehicle] = useState([])
     const [days, setDays] = useState({
-        days:1
+        days:1,
     })
     const { id } = useParams()
     useEffect(() => {
@@ -30,6 +30,14 @@ function BookingPage() {
     const totalDays = (day) => {
         return (days.days * day)
     }
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        axios.post('http://localhost:3000/auth/bookCar')
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((error) => {console.log(error)})
+    }
   return (
     <div className='py-16 max-w-7xl md:w-[90%] mx-auto px-4'>
         <div className='p-4 rounded-md bg-white shadow-md mt-4'>
@@ -40,13 +48,13 @@ function BookingPage() {
                              <img src={`http://localhost:3000/images/`+ item.vehicleImage} alt="" className='w-full object-cover' />
                              <h1 className='text-gray-600 '>{item.vehicleName}</h1>
                              <h1 className='text-gray-600'>{item.vehiclePrice} / Day</h1>
-                             <form action="" className='flex flex-col'>
+                             <form action="" className='flex flex-col' onSubmit={handleSubmit}>
                                 <label htmlFor="" className='text-gray-600'>Rental days</label>
                                 <input type="number" name='days' onChange={handleValueChange} className='border p-2 rounded-md w-[40%] mt-1'/>
                                 <label htmlFor="" className='text-gray-600 mt-2'>Total payout</label>
-                                <input type="number" value={totalDays(item.vehiclePrice)} className='border p-2 rounded-md w-[40%]'/>
+                                <input type="number" name='amount' value={totalDays(item.vehiclePrice)} className='border p-2 rounded-md w-[40%]'/>
+                                <button className='py-2 md:w-[50%] w-full px-12 bg-red-600 text-white cursor-pointer rounded-md mt-4'>Proceed to payment</button>
                              </form>
-                             <button className='py-2 px-12 bg-red-600 text-white cursor-pointer rounded-md cursor-pointer mt-4'>Proceed to payment</button>
                         </div>
                     ))
                 }

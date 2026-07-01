b984e54b2ba6088e1ecdf353b3296f72929f6f1c
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-01 10:52:46 +0300

    Refactoring sql query

--- server/controllers/Usercontrollers.js
+++ server/controllers/Usercontrollers.js
@@ -131,7 +131,7 @@ export const bookCar = (request,response) => {
 
 export const updateBookingstatus = (request,response) => {
     const { id } = request.params
-    const sqlQuerry = "UPDATE vehcles SET vehicleBooking ='Booked' WHERE vehicle_id = ?"
+    const sqlQuerry = "UPDATE vehicles SET vehicleBooking ='Booked' WHERE vehicle_id = ?"
     try {
         conn.query(sqlQuerry,[id],(error,result) => {
             if (error) return response.status(200).json({status: false, message: error})

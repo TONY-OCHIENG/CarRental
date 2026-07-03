9af68cf124ddf4503265fe0ab8218a8b84c96cd2
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-03 15:33:20 +0300

    Implementing profile form

--- client/src/pages/user/UserFeedback.jsx
+++ client/src/pages/user/UserFeedback.jsx
@@ -36,7 +36,6 @@ function UserFeedback() {
     })
     .catch((error) => { console.log(error)})
   }
-  console.log(feedback)
   return (
     <div className='py-16 md:px-10 px-4 flex justify-center items-center'>
       <div className='mt-10 md:w-[50%] rounded-md w-full h-[75vh] p-4 bg-white max-md:overflow-x-auto'>
--- client/src/pages/user/UserProfile.jsx
+++ client/src/pages/user/UserProfile.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 function UserProfile() {
   return (
-    <div>UserProfile</div>
+    <div className='py-16 px-10 flex justify-center items-center w-full h-full'>
+      <div className='w-full md:w-[50%] p-4 bg-white rounded-md shadow-md'>
+        <form action="" className='w-full h-full'>
+          <label htmlFor="firstName">First name</label>
+          <input type="text" className='w-full p-2 border rounded-md '/>
+          <label htmlFor="firstName">Last name</label>
+          <input type="text" className='w-full p-2 border rounded-md '/>
+          <label htmlFor="firstName">Email </label>
+          <input type="text" className='w-full p-2 border rounded-md '/>
+          <button className='w-full p-2 mt-4 font-bold bg-red-600 text-white rounded-md
+          cursor-pointer'>Edit profile</button>
+        </form>
+      </div>
+    </div>
   )
 }
 

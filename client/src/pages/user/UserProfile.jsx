79f16a61749960f0b8e61dbc9f530a9cdbfb32f7
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-03 16:03:47 +0300

    Getting edited values

--- client/src/pages/user/UserProfile.jsx
+++ client/src/pages/user/UserProfile.jsx
@@ -19,21 +19,22 @@ function UserProfile() {
         axios.get(`http://localhost:3000/auth/user/${userId}`)
         .then((response) => {
           if (response.data.status) {
-            setUserDetails(response.data.result)
+            setUserDetails(response.data.result[0])
           }
         })
         .catch((error) => {console.log(error)})
       },[userId])
+      
   return (
     <div className='py-16 md:px-10 px-2 flex justify-center items-center w-full h-full'>
       <div className='w-full md:w-[50%] p-4 bg-white rounded-md shadow-md'>
         <form action="" className='w-full h-full'>
           <label htmlFor="firstName">First name</label>
-          <input type="text" className='w-full p-2 border rounded-md '/>
+          <input value={user.firstName} name='firstName' onChange={(event) => setUserDetails({...user, firstName: event.target.value})} type="text" className='w-full p-2 border rounded-md '/>
           <label htmlFor="firstName">Last name</label>
-          <input type="text" className='w-full p-2 border rounded-md '/>
+          <input value={user.lastName} name='lastName' onChange={(event) => setUserDetails({...user, lastName: event.target.value})}  type="text" className='w-full p-2 border rounded-md '/>
           <label htmlFor="firstName">Email </label>
-          <input type="text" className='w-full p-2 border rounded-md '/>
+          <input value={user.email} name='email' onChange={(event) => setUserDetails({...user, email: event.target.value})}  type="email" className='w-full p-2 border rounded-md '/>
           <button className='w-full p-2 mt-4 font-bold bg-red-600 text-white rounded-md
           cursor-pointer'>Edit profile</button>
         </form>

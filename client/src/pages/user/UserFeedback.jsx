82cf97a2968c05389c0b103d287a9a8a01882eba
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-03 14:50:31 +0300

    Clearing input fields after feedback

--- client/src/pages/user/UserFeedback.jsx
+++ client/src/pages/user/UserFeedback.jsx
@@ -19,28 +19,36 @@ function UserFeedback() {
   }
   const handleSubmit = (event) => {
     event.preventDefault()
-    axios.post('http://localhost:3000/auth/feedback')
+    axios.post('http://localhost:3000/auth/feedback', feedback)
     .then((response) => {
+      console.log(response)
       if (response.data.status) {
         toast.success(response.data.message)
+        setFeedback({
+          firstName:'',
+          lastName:'',
+          email:'',
+          text:''
+        })
       } else {
         toast.error("An error occured")
       }
     })
     .catch((error) => { console.log(error)})
   }
+  console.log(feedback)
   return (
     <div className='py-16 md:px-10 px-4 flex justify-center items-center'>
       <div className='mt-10 md:w-[50%] rounded-md w-full h-[75vh] p-4 bg-white max-md:overflow-x-auto'>
         <form action="" className='w-full h-full' onSubmit={handleSubmit}>
           <label htmlFor="firstName" className='text-md text-gray-600'>First name</label>
-          <input type="text" onChange={handleInput} className='text-gray-400 text-md border w-full p-1 rounded-md mb-1' name='firstName'/>
+          <input type="text" value={feedback.firstName} onChange={handleInput} className='text-gray-400 text-md border w-full p-1 rounded-md mb-1' name='firstName'/>
            <label htmlFor="firstName" className='text-md text-gray-600'>Last name</label>
-          <input type="text" onChange={handleInput} className='text-gray-400 text-md border w-full p-1 rounded-md mb-1' name='lastName'/>
+          <input type="text" value={feedback.lastName} onChange={handleInput} className='text-gray-400 text-md border w-full p-1 rounded-md mb-1' name='lastName'/>
            <label htmlFor="email" className='text-md text-gray-600'>Email</label>
-          <input type="email" onChange={handleInput} className='text-gray-400 text-md border w-full p-1 rounded-md mb-1' name='email'/>
+          <input type="email" value={feedback.email} onChange={handleInput} className='text-gray-400 text-md border w-full p-1 rounded-md mb-1' name='email'/>
           <label htmlFor="text" className='text-md text-gray-600'>Message</label>
-          <textarea name="text" onChange={handleInput} id="text" className='p-2 text-gray-400 text-md w-full border h-[50%] rounded-md'></textarea>
+          <textarea name="text" value={feedback.text} onChange={handleInput} id="text" className='p-2 text-gray-400 text-md w-full border h-[50%] rounded-md'></textarea>
           <button className='w-full p-2 mt-1 bg-red-600 rounded-md font-bold cursor-pointer text-white'>Submit message</button>
         </form>
       </div>

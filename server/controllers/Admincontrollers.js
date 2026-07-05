f586ebd4848f5e3820c36eaa9c6fc80a79133c30
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-05 19:01:20 +0300

    Cancel booking route

--- server/controllers/Admincontrollers.js
+++ server/controllers/Admincontrollers.js
@@ -220,3 +220,17 @@ export const returnbookedvehicle = (request,response) => {
         return response.status(500).json({status: false, message: "Internal server error"})
     }
 }
+
+export const cancelBooking = (request,response) => {
+    const { id } = request.params
+    try {
+        const sqlQuerry = "UPDATE bookings SET bookingStatus = 'Canceled' WHERE vehicle_id = ?"
+        conn.query(sqlQuerry,[id],(error,result) => {
+            if (error) return response.status(200).json({status: false, message: error})
+            return response.status(200).json({status : true, message: "Booking canceled successfully"})
+        })
+    } catch (error) {
+        console.log(error)
+        return response.status(500).json({status: false, message: "Internal server error"})
+    } 
+}

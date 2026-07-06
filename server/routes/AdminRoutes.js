4986e7f682b72424deb42f6b34d28d2d4381b8ad
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-06 16:24:27 +0300

    Refactoring routes

--- server/routes/AdminRoutes.js
+++ server/routes/AdminRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addVehicles, approveBookings, availableVehicles, cancelBooking, deleteVehicle, editVehicle, getAllRepairs, getAllVehicles, getBookings, getsingleVehicle, loginUserAdmin, logoutAdmin, maintenance, repairVehicles, returnbookedvehicle, userRole } from '../controllers/Admincontrollers.js'
+import { addVehicles, approveBookings, availableVehicles, cancelBooking, deleteVehicle, editVehicle, getAllRepairs, getAllVehicles, getBookings, getsingleVehicle, loginUserAdmin, logoutAdmin, maintenance, rentedVehicles, repairVehicles, returnbookedvehicle, userRole } from '../controllers/Admincontrollers.js'
 import { auth } from '../middlewares/auth.js'
 import { upload } from '../configs/imageupload.js'
 
@@ -20,4 +20,5 @@ adminRoute.put('/approveBookings/:id', approveBookings)
 adminRoute.put('/returnBookedvehicle/:id',returnbookedvehicle)
 adminRoute.put('/canceledBooking/:id',cancelBooking)
 adminRoute.get('/availableVehicles',availableVehicles)
+adminRoute.get('rentedvehicles',rentedVehicles)
 export default adminRoute

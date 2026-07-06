1cc6b713be143c166da4add304a4a55bd68c35d5
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-06 16:09:50 +0300

    Counting available vehicles for booking

--- server/controllers/Admincontrollers.js
+++ server/controllers/Admincontrollers.js
@@ -234,3 +234,17 @@ export const cancelBooking = (request,response) => {
         return response.status(500).json({status: false, message: "Internal server error"})
     } 
 }
+
+export const availableVehicles = (request, response) => {
+    try {
+        const sqlQuerry = "SELECT count(*) as count FROM vehicles WHERE vehicleCondition = 'Good' AND  vehicleBooking = 'Free'"
+        conn.query(sqlQuerry,(error,result) => {
+            if (error) return response.status(200).json({status: false, message: error})
+            return response.status(200).json({status: true, result: result})
+        })
+        
+    } catch (error) {
+        console.log(error)
+        return response.status(500).json({status: false, message: "Internal server error"})
+    }
+}

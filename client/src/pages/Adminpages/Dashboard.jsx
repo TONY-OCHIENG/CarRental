18ec0557dfa051ba03a96737da34df8d92e33de2
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-06 16:54:54 +0300

    Counting overdue vehicles

--- client/src/pages/Adminpages/Dashboard.jsx
+++ client/src/pages/Adminpages/Dashboard.jsx
@@ -19,6 +19,7 @@ function Dashboard() {
   const [availableVehicles, setAvailableVehicles] = useState(null)
   const [rent,setRent] = useState(null)
   const [check, setCheck] = useState(null)
+  const [overdue, setOverdue] = useState(null)
   let reminderValues = JSON.parse(localStorage.getItem('reminder')) || []
   const handleSubmit = (event) => {
     const { name } = value
@@ -58,6 +59,13 @@ function Dashboard() {
       }
     })
     .catch((error) => {console.log(error)})
+    axios.get('http://localhost:3000/auth/overdue')
+    .then((response) => {
+      if (response.data.status) {
+          setOverdue(response.data.result)
+      }
+    })
+    .catch((error) => {console.log(error)})
   },[])
   return (
     <div className='h-full w-full py-20 '>
@@ -92,7 +100,7 @@ function Dashboard() {
           <h1 className='text-center font-extrabold'>Overdue Vehicles</h1>
           <div className='flex items-center ml-5'>
             <ClockAlert className='h-8 w-8 text-red-600'/>
-            <h1 className='text-md ml-5 text-gray-600'>1 units</h1>
+            <h1 className='text-md ml-5 text-gray-600'>{overdue} units</h1>
           </div>
          </div>
           <div className='rounded-md p-4 shadow-md cursor-pointer hover:-translate-y-0.5 transition-all duration-300

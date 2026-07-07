211c678cfccf8aa41392667b65b1ba8c40da142a
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-07 15:57:36 +0300

    Displaying the number of vehicles on a pie chart

--- client/src/pages/Adminpages/Report.jsx
+++ client/src/pages/Adminpages/Report.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import { Chart as Chartjs} from 'chart.js/auto'
-import { Doughnut } from 'react-chartjs-2'
+import { Doughnut, Pie } from 'react-chartjs-2'
 
 function Report() {
   const [category,setCategory] = useState([])
@@ -21,7 +21,17 @@ function Report() {
     <div className='py-10 px-4 w-full h-full'>
       <div className='mt-8 w-full h-full rounded-md mx-auto flex justify-center items-center flex-col'>
         <div className='flex gap-6 flex-col md:flex-row'>
-          <div className='w-[500px] h-[300px] shadow-md bg-white rounded-md'></div>
+          <div className='w-[500px] h-[300px] shadow-md bg-white rounded-md flex justify-center'>
+               <Pie className='w-full' data = {{
+                                datasets: [{
+                                    data: category.map((item) => item.total),
+                                    backgroundColor:["blue",'pink','orange']
+                                  }],
+            
+                                // These labels appear in the legend and in the tooltips when hovering different arcs
+                                labels: category.map((item) => item.vehicleType),
+                            }}/>  
+          </div>
           <div className='w-[500px] h-[300px] shadow-md bg-white rounded-md'></div>
         </div>
         <div className='w-full md:w-[82%] h-[350px] mt-2 shadow-md bg-white rounded-md'></div>

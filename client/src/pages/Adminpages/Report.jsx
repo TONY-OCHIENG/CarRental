ac7d00959ff7fc6aa77ff4bf207ba20e692eb6c5
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-07 15:37:09 +0300

    Adding cards in reports section

--- client/src/pages/Adminpages/Report.jsx
+++ client/src/pages/Adminpages/Report.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Report() {
   return (
-    <div>Report</div>
+    <div className='py-10 px-4 w-full h-full'>
+      <div className='mt-8 w-full h-full rounded-md mx-auto flex justify-center items-center flex-col'>
+        <div className='flex gap-6 flex-col md:flex-row'>
+          <div className='w-[500px] h-[300px] shadow-md bg-white rounded-md'></div>
+          <div className='w-[500px] h-[300px] shadow-md bg-white rounded-md'></div>
+        </div>
+        <div className='w-full md:w-[82%] h-[350px] mt-2 shadow-md bg-white rounded-md'></div>
+      </div>
+    </div>
   )
 }
 

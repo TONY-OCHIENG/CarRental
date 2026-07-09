ed92139d30167f7c0be26cbd471d955f6666b521
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-09 12:11:38 +0300

    Refactoring home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Home() {
   return (
-    <div id='home' className='h-[100vh] w-full bg-gray-400'>Home</div>
+    <div id='home' className='py-16 w-full h-screen  bg-white'>
+        <div className='max-w-7xl md:w-[80%] mx-auto w-full h-full px-4 flex md:flex-row flex-col justify-between'>
+            <div className='mt-40'>
+             <h1 className='font-extrabold md:text-5xl text-gray-600 text-4xl'>Find Affordable Dream <br /> Car For Rent</h1>
+             <p className='mt-10 text-gray-400'>Fulfill your automotive fantacies without breaking the bank. Check <br />
+             our affordable car rentals for an opulent yet economical ride </p>
+             <div className='flex gap-10'>
+                <button className='mt-10 px-8 py-2 rounded-full cursor-pointer border bg-red-600 text-white font-extrabold'>Get in Touch</button>
+                <button className='mt-10 px-16 py-2 rounded-full cursor-pointer border font-extrabold'>Login</button>
+             </div>
+             <p className='text-sm text-gray-400 mt-10'>Over 1000+ satisfied clients</p>
+            </div>
+            <div>
+                <img src="/src/assets/hero2.jpg" alt="" />
+            </div>
+        </div>      
+    </div>
   )
 }
 
